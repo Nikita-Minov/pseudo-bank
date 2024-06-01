2a6fd5a13dfaa20a31d1764de789cc449df72098
Nikita-Minov <nikitachryackhov555@gmail.com>
2024-06-01 23:46:39 +0300

    Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import {Avatar, Col, Flex, Row} from "antd";
 import styles from "../../assets/styles/components/header.module.css";
 import {Link} from "react-router-dom";
 import AvatarIcon from "./AvatarIcon.tsx";
+import logoutSvg from '../../assets/img/logout.svg';
 
 const Header = () => {
 	return (
@@ -15,7 +16,13 @@ const Header = () => {
 					</Flex>
 				</Col>
 				<Col xl={4}>
-					<Avatar size={48} icon={<AvatarIcon/>} />
+					<Flex justify="center" align="center">
+						<p className={styles.header__username}>Хрячков Никита</p>
+						<Avatar size={48} icon={<AvatarIcon/>}/>
+						<span className={styles.header__logout} >
+							<img src={logoutSvg} alt="logout"/>
+						</span>
+					</Flex>
 				</Col>
 			</Row>
 		</>
