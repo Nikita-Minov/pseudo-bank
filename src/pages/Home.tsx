d7932a9cb0934e54976f330f49251e1baddbd646
Nikita-Minov <nikitachryackhov555@gmail.com>
2024-06-04 23:54:45 +0300

    Added props for transfer form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,37 @@
 import MoneyTransfer from "moneyTransfer/MoneyTransfer";
 import {Col, Row} from "antd";
+import {useState} from "react";
+
+interface Recipient {
+	value: string;
+	label: string;
+}
+
+interface Transfer {
+	recipient: string;
+	date: Date;
+	sum: string;
+}
+
 
 function Home() {
+	const recipients: Array<Recipient> = [
+		{ value: 'Иванов Иван', label: 'Иванов Иван' },
+		{ value: 'Петров Пётр', label: 'Петров Пётр' },
+	];
+
+	const [transfers, setTransfers] = useState<Array<Transfer>>([]);
+
+	const addTransfer = (transfer: Transfer) => {
+		setTransfers([...transfers, transfer]);
+		alert(JSON.stringify(transfer));
+	}
+
+
 	return (
 		<Col xl={20}>
 			<Row style={{height: '93vh'}} align="middle" justify="center">
-				<MoneyTransfer/>
+				<MoneyTransfer addTransfer={addTransfer} recipients={recipients}/>
 			</Row>
 		</Col>
 	)
